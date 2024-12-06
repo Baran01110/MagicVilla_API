e7b673c5501468defd774605aab8b96ce3fb134e
Baran01110 <baran.abayy@gmail.com>
2024-12-07 00:10:53 +0300

    Clear default class. Add some folders and classes. Creat Get api.

---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto> {
+                new VillaDto{ID =1, Name = "Pool View"},
+                new VillaDto{ID =2, Name = "Beach View"}
+            };
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int ID { get; set; }   
+        public string Name { get; set; }
+    }
+}
