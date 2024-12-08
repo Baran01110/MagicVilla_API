abd47e67c0c013eb76ddff2a813759c71c62cdb4
Baran01110 <baran.abayy@gmail.com>
2024-12-08 19:35:24 +0300

    HttpPost CreateVilla Created

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -10,15 +10,45 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDto> GetVillas() 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas() 
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
-        
-        [HttpGet("id")]
-        public VillaDto GetVillas(int id) 
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult<VillaDto> GetVillas(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u=>u.ID==id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.ID == id);
+            if (villa == null) { return NotFound(); }
+            return Ok(villa);
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CreateVilla([FromBody]VillaDto villaDto) 
+        {
+         if(villaDto == null) 
+            {
+                return BadRequest(villaDto);
+            }
+         if(villaDto.ID > 0) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+          villaDto.ID = VillaStore.villaList.OrderByDescending(u => u.ID).FirstOrDefault().ID+1;
+            VillaStore.villaList.Add(villaDto);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDto.ID }, villaDto);
+       }
     }
 }
